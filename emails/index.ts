08ab6917d2eeb20d901107b5efc1fae45682d2f2
Tom Esterez <testerez@shutterstock.com>
2022-12-08 09:42:23 +0100

    add html-to-text KO

--- emails/index.ts
+++ emails/index.ts
@@ -1,6 +1,19 @@
 import nodemailer from "nodemailer";
 import { buildSendMail } from "mailing-core";
 
+import { htmlToText } from 'html-to-text'
+
+export const emailHtmlToText = (html: string) => {
+  return htmlToText(html, {
+    whitespaceCharacters: ' \t\r\n\f\u200b\u200A\u00A0\u202f',
+    selectors: [
+      { selector: 'img', format: 'skip' },
+      { selector: 'div[style*="display:none"i]', format: 'skip' },
+      { selector: '[data-table]', format: 'dataTable' },
+    ],
+  })
+}
+
 const transport = nodemailer.createTransport({
   pool: true,
   host: "smtp.example.com",
